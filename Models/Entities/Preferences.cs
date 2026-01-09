5b6558db43a1bb430f57f56027b2d606ab02a524
Bia104 <cerinobia@gmail.com>
2026-01-09 19:21:02 +0100

    Update Preferences

--- Models/Entities/Preferences.cs
+++ Models/Entities/Preferences.cs
@@ -6,15 +6,22 @@ namespace eppoi.Models.Entities
     [Flags]
     public enum Preferences
     {
-        P_ArtCultures = 1 << 0,
-        P_Natures = 1 << 1,
-        P_Entertainments = 1 << 2,
-        P_Restaurants = 1 << 3,
-        P_Sleeps = 1 << 4,
-        P_Events = 1 << 5,
-        P_Routes = 1 << 6,
-        P_Articles = 1 << 7,
-        P_Shoppings = 1 << 8,
-        P_Organizations = 1 << 9,
+        P_ArtCulture = 1 << 0,
+        P_Nature = 1 << 1,
+        P_Entertainment = 1 << 2,
+        P_Restaurant = 1 << 3,
+        P_Sleep = 1 << 4,
+        P_Event = 1 << 5,
+        P_Route = 1 << 6,
+        P_Article = 1 << 7,
+        P_Shopping = 1 << 8,
+        P_Organization = 1 << 9,
+        T_Solo = 1 << 10,
+        T_Couple = 1 << 11,
+        T_Family = 1 << 12,
+        T_Friends = 1 << 13,
+        F_GlutenFree = 1 << 14,
+        F_DairyFree = 1 << 15,
+        F_Vegetarian = 1 << 16
     }
 }
